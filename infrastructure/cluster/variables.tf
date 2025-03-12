0d59ecf48a2f3ce75510d1aa86621a4a5d51ed69
jolfr <thomas.jack.carroll@gmail.com>
2025-03-12 18:40:55 -0400

    feat(infra): added provider configuration

---
+++ infrastructure/cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "GCP project to deploy resources within"
+  type = string
+  default = "data-project-example"
+}
+
+variable "region" {
+  description = "GCP region to deploy resources within"
+  type = string
+  default = "us-east4"
+}
