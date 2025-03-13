7d3196e6109283fb09e0ae1b5254d4843bdbd386
jolfr <thomas.jack.carroll@gmail.com>
2025-03-13 14:01:47 -0400

    feat(infra): added initial definition for general node pool

--- infrastructure/cluster/main.tf
+++ infrastructure/cluster/main.tf
@@ -1,3 +1,8 @@
+resource "google_service_account" "nodes" {
+  account_id   = local.pool_service_account_id
+  display_name = var.pool_service_account_name
+}
+
 resource "google_container_cluster" "main" {
   name     = local.cluster_name
   location = var.region
@@ -10,4 +15,27 @@ resource "google_container_cluster" "main" {
 
   # Allow cluster to be deleted by terraform
   deletion_protection = false
+}
+
+resource "google_container_node_pool" "general_nodes" {
+  name       = local.general_pool_name
+  location   = var.region
+  cluster    = google_container_cluster.main.id
+
+  initial_node_count = 1
+
+  autoscaling {
+    total_min_node_count = var.general_pool_min_nodes
+    total_max_node_count = var.general_pool_max_nodes
+  }
+
+  node_config {
+    machine_type = "e2-small"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.nodes.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
--- infrastructure/cluster/variables.tf
+++ infrastructure/cluster/variables.tf
@@ -1,5 +1,7 @@
 locals {
   cluster_name = "${var.project}-cluster"
+  general_pool_name = "${var.project}-gen-np"
+  pool_service_account_id = "${var.project}-node-sa"
 }
 
 variable "project" {
@@ -12,4 +14,22 @@ variable "region" {
   description = "GCP region to deploy resources within"
   type = string
   default = "us-east4"
+}
+
+variable "general_pool_min_nodes" {
+  description = "Minimum number of nodes in the general node pool"
+  type = number
+  default = 0
+}
+
+variable "general_pool_max_nodes" {
+  description = "Maximum number of nodes in the general node pool"
+  type = number
+  default = 10
+}
+
+variable "pool_service_account_name" {
+  description = "Service account name for node pool machines"
+  type = string
+  default = "Cluster Nodes Service Account"
 }
