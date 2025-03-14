ba720860d3eff3d1444e26352b81bc8609e8c148
Thomas Jack Carroll <thomas.jack.carroll@gmail.com>
2025-03-13 20:52:39 -0400

    feat(infra): added creation of namespace

--- infrastructure/argocd/variables.tf
+++ infrastructure/argocd/variables.tf
@@ -2,3 +2,8 @@ variable "cluster_terraform_workspace" {
   description = "Terraform Cloud workspace for the cluster"
   default     = "data-project-example-cluster"
 }
+
+variable "kubernetes_namespace" {
+  description = "Kubernetes namespace to create and use for Argo CD deployment"
+  default = "argo-cd"
+}
