737184dd10d1f0e8865679e37a3833e24977aa9d
Thomas Jack Carroll <thomas.jack.carroll@gmail.com>
2025-03-18 16:47:24 -0400

    fix(infra): add lookup for global static IP in ArgoCD data.tf
    Signed-off-by: Thomas Jack Carroll <thomas.jack.carroll@gmail.com>

--- infrastructure/argo-cd/data.tf
+++ infrastructure/argo-cd/data.tf
@@ -13,3 +13,8 @@ data "google_client_config" "default" {}
 data "google_container_cluster" "main" {
   name = data.tfe_outputs.cluster.nonsensitive_values.cluster_name
 }
+
+data "google_compute_address" "global_static_ip" {
+  name   = "${data.tfe_outputs.networking.nonsensitive_values.project}-cd"
+  region = data.tfe_outputs.networking.nonsensitive_values.region
+}
