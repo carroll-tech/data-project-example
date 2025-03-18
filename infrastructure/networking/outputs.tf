d930a62d786c12733b1e969d7a0b325419643307
Thomas Jack Carroll <thomas.jack.carroll@gmail.com>
2025-03-18 14:58:14 -0400

    refactor(infra): consolidate all static IP outputs into static_ip_details map
    Signed-off-by: Thomas Jack Carroll <thomas.jack.carroll@gmail.com>

--- infrastructure/networking/outputs.tf
+++ infrastructure/networking/outputs.tf
@@ -8,35 +8,6 @@ output "region" {
   value       = var.region
 }
 
-output "static_ip_names" {
-  description = "The names of the static IP addresses"
-  value       = google_compute_address.static_ip[*].name
-}
-
-output "static_ip_addresses" {
-  description = "The actual IP addresses allocated"
-  value       = google_compute_address.static_ip[*].address
-}
-
-output "static_ip_self_links" {
-  description = "The URI of the created resources"
-  value       = google_compute_address.static_ip[*].self_link
-}
-
-output "subdomains" {
-  description = "The subdomains used for the static IPs"
-  value       = local.subdomain_names
-}
-
-output "domain_names" {
-  description = "The full domain names (e.g., cd.data-project-example.net)"
-  value       = [for subdomain in local.subdomain_names : "${subdomain}.${local.domain_base}"]
-}
-
-output "https_urls" {
-  description = "The HTTPS URLs for the domains (e.g., https://cd.data-project-example.net/)"
-  value       = [for subdomain in local.subdomain_names : "https://${subdomain}.${local.domain_base}/"]
-}
 
 # DNS outputs are commented out since DNS resources are not being created
 # Uncomment if you enable the DNS resources
@@ -59,28 +30,20 @@ output "dns_records" {
 }
 */
 
-# Alternative DNS-like output that doesn't depend on actual DNS resources
-output "domain_ip_mapping" {
-  description = "Map of domains to their corresponding IP addresses (for manual DNS configuration)"
-  value = {
-    for i, subdomain in local.subdomain_names :
-    subdomain => {
-      domain = "${subdomain}.${local.domain_base}"
-      ip     = google_compute_address.static_ip[i].address
-      url    = "https://${subdomain}.${local.domain_base}/"
-    }
-  }
-}
-
-# Map of static IP names to their details (FQDN, IP address, description)
+# Comprehensive map of static IP details
 output "static_ip_details" {
-  description = "Map of static IP names to their FQDN, IP address, and description"
+  description = "Map of static IP names to their complete details"
   value = {
     for i, ip in google_compute_address.static_ip :
     ip.name => {
       fqdn        = "${var.subdomains[i].name}.${local.domain_base}"
       ip_address  = ip.address
       description = ip.description
+      self_link   = ip.self_link
+      subdomain   = var.subdomains[i].name
+      https_url   = "https://${var.subdomains[i].name}.${local.domain_base}/"
+      network_tier = var.subdomains[i].network_tier
+      address_type = var.subdomains[i].address_type
     }
   }
   sensitive = true
