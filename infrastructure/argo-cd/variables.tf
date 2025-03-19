92afe7ccd41fab09b450bb37322778747db8a57d
Thomas Jack Carroll <thomas.jack.carroll@gmail.com>
2025-03-19 04:09:50 -0400

    fix(infra): remove argocd_admin_password passthrough
    Signed-off-by: Thomas Jack Carroll <thomas.jack.carroll@gmail.com>

--- infrastructure/argo-cd/variables.tf
+++ infrastructure/argo-cd/variables.tf
@@ -22,9 +22,3 @@ variable "helm_chart_version" {
   description = "Version of the helm chart to use for Argo CD deployment"
   default     = "7.8.10"
 }
-
-variable "argocd_admin_password" {
-  description = "Admin password for Argo CD"
-  type        = string
-  sensitive   = true
-}
